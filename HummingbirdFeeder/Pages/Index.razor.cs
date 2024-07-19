7747fa97a46003078588cc34ff87175d1b6facde
E.M. Coleman <mlecoleman@gmail.com>
2024-07-19 19:42:25 -0400

    Add inital attempt at core logic for feeder change

--- HummingbirdFeeder/Pages/Index.razor.cs
+++ HummingbirdFeeder/Pages/Index.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using HummingbirdFeeder.Data;
 using System.Text.Json;
+using System.Globalization;
 using System.Linq;
 using HummingbirdFeeder.Models;
 using System.Diagnostics;
@@ -96,16 +97,71 @@ namespace HummingbirdFeeder.Pages
             await ShowFeeders();
         }
 
-        public async Task GetTemperatureMax()
+        // API and feeeder change logic
+
+        public async Task<List<string>> GetListOfDatesSinceLastChangeDate(Feeder myFeeder)
         {
-            string zipcode = "40204";
+            List <string> datesSinceLastFeederChange = new List<string>();
+
+            string lastChangeDate = (myFeeder.LastChangeDate).ToString();
+            DateTime changeDate = DateTime.ParseExact(lastChangeDate, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            DateTime today = (DateTime.Now.Date);
+            string formattedDate = today.ToString("yyyy-MM-dd");
+
+            _context ??= await FeederDataContextFactory.CreateDbContextAsync();
+            if (_context is not null)
+            {
+                for (DateTime date = changeDate; date <= today; date = date.AddDays(1))
+                {
+                    string dateString = date.ToString("yyyy-MM-dd");
+                    datesSinceLastFeederChange.Add(dateString);
+                }
+            }
+
+            return datesSinceLastFeederChange;
+        }
+
+        public async Task<List<double>> GetListOfTemperatureMaxPerDate(Feeder myFeeder)
+        {
+            string zipcode = (myFeeder.Zipcode).ToString();
+            int lastChangeDate = myFeeder.LastChangeDate;
+            Task<List<string>> datesSinceLastFeederChange = GetListOfDatesSinceLastChangeDate(myFeeder);
             string todaysDate = DateTime.Now.ToString("yyyy-MM-dd");
-            string key = "";
-            string urlSuffix = $"v1/history.json?key={key}&q={zipcode}&dt={todaysDate}";
-            var response = await _client.GetAsync(urlSuffix);
-            var rawJson = await response.Content.ReadAsStringAsync();
-            Root rootObject = JsonSerializer.Deserialize<Root>(rawJson);
-            maxTemp = rootObject.forecast.forecastday[0].day.maxtemp_f;
+            string key = "e44d36a439384f149d9182816241307";
+            List<double> maxTemperaturesPerDay = new List<double>();
+            _context ??= await FeederDataContextFactory.CreateDbContextAsync();
+            if (_context is not null)
+            {
+                foreach (string date in await datesSinceLastFeederChange)
+                {
+                    string urlSuffix = $"v1/history.json?key={key}&q={zipcode}&dt={date}";
+                    var response = await _client.GetAsync(urlSuffix);
+                    var rawJson = await response.Content.ReadAsStringAsync();
+                    Root rootObject = JsonSerializer.Deserialize<Root>(rawJson);
+                    maxTemp = rootObject.forecast.forecastday[0].day.maxtemp_f;
+                    maxTemperaturesPerDay.Add(maxTemp);
+                }
+            }
+            return maxTemperaturesPerDay;
+        }
+
+        public async Task<bool> DoesFeederNeedToBeChanged(Feeder myFeeder)
+        {
+            Task<List<double>> dailyHighTemps = GetListOfTemperatureMaxPerDate(myFeeder);
+            double maxTemp = (await dailyHighTemps).Max();
+            int daysSinceChange = (await dailyHighTemps).Count();
+            bool changeFeeder;
+
+            if (maxTemp <= 70 && daysSinceChange >= 7) changeFeeder = true;
+            else if (maxTemp > 70 && maxTemp <= 75 && daysSinceChange >= 6) changeFeeder = true;
+            else if (maxTemp > 75 && maxTemp <= 80 && daysSinceChange >= 5) changeFeeder = true;
+            else if (maxTemp > 80 && maxTemp <= 84 && daysSinceChange >= 4) changeFeeder = true;
+            else if (maxTemp > 84 && maxTemp <= 88 && daysSinceChange >= 3) changeFeeder = true;
+            else if (maxTemp > 88 && maxTemp <= 92 && daysSinceChange >= 2) changeFeeder = true;
+            else if (maxTemp > 92 && daysSinceChange >= 1) changeFeeder = true;
+            else changeFeeder = false;
+
+            return changeFeeder;
         }
     }
 }
